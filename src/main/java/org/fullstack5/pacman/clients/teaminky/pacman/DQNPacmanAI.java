50ad075d14d9295255099302e7b3a565731e7417
Rodrigo García <rodrigo.garcia.leon@ing.nl>
2018-08-05 15:42:39 +0200

    Decrease replay size

--- src/main/java/org/fullstack5/pacman/clients/teaminky/pacman/DQNPacmanAI.java
+++ src/main/java/org/fullstack5/pacman/clients/teaminky/pacman/DQNPacmanAI.java
@@ -15,7 +15,7 @@ import java.util.List;
 public class DQNPacmanAI {
     private static final float epsFinal = 0.1f;
     private static final float epsStep = 10000.0f;
-    private static final int replayMemorySize = 100000;
+    private static final int replayMemorySize = 10000;
     private static final int trainingStart = 5000;
     private static final int batchSize = 32;
     private static final float REWARD_WON = 100.0f;
